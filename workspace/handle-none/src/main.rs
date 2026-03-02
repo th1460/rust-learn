0757a14718475c77d1367fd801fb28456b5f43ad
Thiago-Pires <thiago.pires@ibm.com>
2026-03-02 15:49:43 -0300

    feat: handle-none example

--- workspace/handle-none/src/main.rs
+++ workspace/handle-none/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+fn plus_one(input: Option<i32>) -> i32 {
+    match input {
+        Some(value) => {
+            // Return the unwrapped 'value' of type i32
+            value + 1
+        },
+        None => {
+            // Handle the absence of a value by returning a default i32
+            0
+        }
+    }
 }
+
+fn main() {
+    let six = plus_one(Some(5));
+    println!("{}", six);
+    let none = plus_one(None);
+    println!("{}", none);
+}
+
